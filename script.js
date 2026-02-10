eb9672f08e0438345275ab407b7e61bbfb99a01c
harshaddddd <cashwithharsh@gmail.com>
2026-02-10 21:32:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-// smooth scroll
-document.querySelectorAll("nav a").forEach(link=>{
-  link.addEventListener("click", function(e){
-    e.preventDefault();
-    document.querySelector(this.getAttribute("href"))
-      .scrollIntoView({behavior:"smooth"});
-  });
+document.getElementById("enquiryForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  fetch("https://script.google.com/macros/s/AKfycbxCLH_NK5SJvzHY2l0yU2BpsapnDWMVt2DC2Xtm84ZbuJzE9MQ0P71pY832zwh0BNXe/execL", {
+    method: "POST",
+    body: JSON.stringify({
+      name: document.getElementById("name").value,
+      date: document.getElementById("date").value,
+      guests: document.getElementById("guests").value,
+      phone: document.getElementById("phone").value
+    })
+  })
+  .then(res => alert("Request submitted successfully!"))
+  .catch(err => alert("Error submitting form"));
 });
