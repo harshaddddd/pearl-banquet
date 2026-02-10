4cdf118975c5ffd0bf9a5c3a73dd2a7a08825283
harshaddddd <cashwithharsh@gmail.com>
2026-02-10 21:16:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// smooth scroll
+document.querySelectorAll("nav a").forEach(link=>{
+  link.addEventListener("click", function(e){
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href"))
+      .scrollIntoView({behavior:"smooth"});
+  });
+});
