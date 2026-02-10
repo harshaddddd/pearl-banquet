3be38d731fb5793f4f4ca249012e1dc752d41222
harshaddddd <cashwithharsh@gmail.com>
2026-02-10 21:40:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById("enquiryForm").addEventListener("submit", function(e) {
   e.preventDefault();
 
-  fetch("https://script.google.com/macros/s/AKfycbxCLH_NK5SJvzHY2l0yU2BpsapnDWMVt2DC2Xtm84ZbuJzE9MQ0P71pY832zwh0BNXe/execL", {
+  fetch("https://script.google.com/macros/s/AKfycbxCLH_NK5SJvzHY2l0yU2BpsapnDWMVt2DC2Xtm84ZbuJzE9MQ0P71pY832zwh0BNXe/exec", {
     method: "POST",
     body: JSON.stringify({
       name: document.getElementById("name").value,
@@ -13,3 +13,4 @@ document.getElementById("enquiryForm").addEventListener("submit", function(e) {
   .then(res => alert("Request submitted successfully!"))
   .catch(err => alert("Error submitting form"));
 });
+
